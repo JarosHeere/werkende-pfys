f90b48b41f481196bd9a689fc5ede03c6fc1e40b
Agie123 <ahmetcan_@live.nl>
2017-01-13 13:50:24 +0100

    fdfv

---
+++ werkende-pfys-master/src/pyfs/Found.java
@@ -0,0 +1,218 @@
+package pyfs;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+
+/**
+ *
+ * @author merij
+ */
+public class Found {
+    
+  
+    private TextField time, labelnr, flightnr, destin, nametrav, lugtype, lugbrand, lugcolor, lugweight, date;
+    private TextField airport;
+    private TextArea lugspef;
+    
+    Found() {
+
+    }
+
+    public TextField getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time.getText().toLowerCase();
+    }
+
+    public String getLabelnr() {
+        return labelnr.getText().toLowerCase();
+    }
+
+    public String getFlightnr() {
+        return flightnr.getText().toLowerCase();
+    }
+
+    public String getDestin() {
+        return destin.getText().toLowerCase();
+    }
+
+    public String getNametrav() {
+        return nametrav.getText().toLowerCase();
+    }
+
+    public String getLugtype() {
+        return lugtype.getText().toLowerCase();
+    }
+
+    public String getLugbrand() {
+        return lugbrand.getText().toLowerCase();
+    }
+
+    public String getLugcolor() {
+        return lugcolor.getText().toLowerCase();
+    }
+
+    public String getLugweight() {
+        return lugweight.getText().toLowerCase();
+    }
+
+    public String getAirport() {
+        return airport.getText().toLowerCase();
+    }
+
+    public String getLugspef() {
+        return lugspef.getText().toLowerCase();
+    }
+
+    
+    
+    TextField date() {
+
+        date = new TextField();
+        date.setPromptText("YYYY-MM-DD");
+        date.setMaxWidth(220);
+        date.setTranslateY(-50);
+
+        return date;
+    }
+
+    TextField Time() {
+
+        time = new TextField();                 //text voor tijd invullen
+        time.setPromptText("Time (HH:MM)");
+        time.setFont(Font.font("Verdana", 20));
+        time.setMaxWidth(220);
+        time.setTranslateY(0);
+
+        return time;
+    }
+
+    TextField airport() {
+
+        airport = new TextField();                 //text voor vliegveld
+        airport.setPromptText("Airport");
+        airport.setFont(Font.font("Verdana", 20));
+        airport.setTranslateY(50);
+        airport.setMaxWidth(220);
+     
+
+        return airport;
+
+    }
+    
+     TextField Labelnr() {
+
+        labelnr = new TextField();                 //text voor label invullen
+        labelnr.setPromptText("Label number");
+        labelnr.setFont(Font.font("Verdana", 20));
+        labelnr.setMaxWidth(220);
+        labelnr.setTranslateY(-50);
+
+        return labelnr;
+    }
+
+    TextField Flightnr() {
+        flightnr = new TextField();                 //text voor vluchtnummer invullen
+        flightnr.setPromptText("Flight number");
+        flightnr.setFont(Font.font("Verdana", 20));
+        flightnr.setMaxWidth(220);
+        flightnr.setTranslateY(0);
+
+        return flightnr;
+    }
+
+    TextField Destin() {
+        destin = new TextField();                 //text voor bestemming invullen
+        destin.setPromptText("Destination");
+        destin.setFont(Font.font("Verdana", 20));
+        destin.setMaxWidth(220);
+        destin.setTranslateY(50);
+
+        return destin;
+    }
+    
+    TextField NameTrav() {
+        
+        nametrav = new TextField();             //text voor naam invullen
+        nametrav.setPromptText("Name Traveller");
+        nametrav.setFont(Font.font("Verdana", 20));
+        nametrav.setMaxWidth(220);
+        nametrav.setTranslateY(50);
+        
+        
+        return nametrav;
+    }
+    
+     TextField Lugtype() {
+
+        lugtype = new TextField();                 //text voor bagage soort invullen
+        lugtype.setPromptText("Lugage type");
+        lugtype.setFont(Font.font("Verdana", 20));
+        lugtype.setMaxWidth(300);
+        lugtype.setTranslateY(-200);
+
+        return lugtype;
+    }
+
+    TextField Lugbrand() {
+        lugbrand = new TextField();                 //text voor band invullen
+        lugbrand.setPromptText("Lugage brand");
+        lugbrand.setFont(Font.font("Verdana", 20));
+        lugbrand.setMaxWidth(300);
+        lugbrand.setTranslateY(-150);
+
+        return lugbrand;
+    }
+
+    TextField Lugcolor() {
+        lugcolor = new TextField();                 //text voor tas kleur invullen
+        lugcolor.setPromptText("Lugage color");
+        lugcolor.setFont(Font.font("Verdana", 20));
+        lugcolor.setMaxWidth(300);
+        lugcolor.setTranslateY(-100);
+
+        return lugcolor;
+    }
+
+    TextField Lugweight() {
+        lugweight = new TextField();                 //text voor gewicht invullen
+        lugweight.setPromptText("Lugage weight");
+        lugweight.setFont(Font.font("Verdana", 20));
+        lugweight.setMaxWidth(300);
+        lugweight.setTranslateY(-50);
+
+        return lugweight;
+    }
+
+    TextArea Lugspef() {                    // text voor bijzondere eigenschappen
+        lugspef = new TextArea();
+        lugspef.setPromptText("Lugage specifications");
+        lugspef.setFont(Font.font("Verdana", 20));
+        lugspef.setMaxSize(300, 160);
+        lugspef.setTranslateY(60);
+
+        return lugspef;
+    }
+    
+    public void Clear(){
+        time.setText("");
+        labelnr.setText("");
+        flightnr.setText("");
+        destin.setText("");
+        nametrav.setText("");
+        lugtype.setText("");
+        lugbrand.setText("");
+        lugcolor.setText("");
+        lugweight.setText("");
+        date.setText("");
+        airport.setText("");
+        lugspef.setText("");
+        
+    }
+    
+}
---
+++ werkende-pfys-master/src/pyfs/Foundd.java
@@ -0,0 +1,120 @@
+package pyfs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author merij
+ */
+public class Foundd {
+
+    mysql Mysql = new mysql();
+    Found found1 = new Found();
+
+    public Foundd() {
+    }
+
+    public void getLuggage(String [] foundbagage) {
+
+        Connection conn;                                                            //making connection to database
+
+        final String USERNAME = Mysql.username();
+        final String PASSWORD = Mysql.password();
+        final String CONN_STRING = Mysql.urlmysql();
+        
+        int Pnr = 2;
+        int Unr = 2;
+        int Lugagenr = 2;
+
+       
+        try {
+
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement st = conn.createStatement();
+            
+
+            String query = "INSERT INTO lugage (Lugagelnr, Lugagetype, Lugagebrand, Lugagecol, Lugeweight, Lugagewespef, Unr,Pnr, LFDM) VALUES (" + '"' + Lugagenr + '"' + "," + '"' + foundbagage[0] + '"' + "," + '"' + foundbagage[1] + '"' + "," + '"'
+                    + foundbagage[2] + '"' + "," + '"' + foundbagage[3] + '"' + "," + '"' + foundbagage[4]  +'"' + "," + '"' + Unr  + '"' +  "," + '"' + Pnr  +'"' + ",'F'" + " )";
+
+            st.executeUpdate(query);
+
+        } catch (SQLException ed) {
+
+            System.err.println(ed);
+
+        }
+
+    }
+
+    public void getDate(String[] datex) {
+
+        Connection conn;                                                            //making connection to database
+
+        final String USERNAME = Mysql.username();
+        final String PASSWORD = Mysql.password();
+        final String CONN_STRING = Mysql.urlmysql();
+
+
+        try {
+
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement st = conn.createStatement();
+            
+            
+
+            String query = ("INSERT INTO dta (date, time, airport, Unr) VALUES (" + '"' + datex[0] + '"' + "," + '"' + datex[1] + '"' + "," + '"' + datex[2] + '"' + " ,100)");
+
+            //String query = "INSERT INTO dta (date, time, airport, Unr) VALUES (" + '"' + datex[0] + '"' + "," + '"' + datex[1] + '"' + "," + '"' + datex[2] + '"' + "," + '"' + 100 + '"' + " )";
+
+            st.executeUpdate(query);
+        }
+        catch (SQLException ed) {
+
+            System.err.println(ed);
+
+        }
+
+    }
+
+ public void getLabel(String[] info) {
+
+        Connection conn;                                                            //making connection to database
+
+        final String USERNAME = Mysql.username();
+        final String PASSWORD = Mysql.password();
+        final String CONN_STRING = Mysql.urlmysql();
+
+
+        try {
+
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement st = conn.createStatement();
+            
+            
+
+            String query = ("INSERT INTO flight (Unr, labelnr, flightnr, destin) VALUES (" + '"' + 100 + '"' + "," + "'" + info[0] + "'" + "," + '"' + info[1] + '"' + "," + '"' +  null + '"' + ")");
+            //String query2 = ("INSERT into persoon (Pnr, name, adress, city, zip, country, tel, mail) VALUES (25, " + '"' + info[2] + '"' + ", null, null, null, null, null, null)");
+
+           
+
+            st.executeUpdate(query);
+         //  st.executeUpdate(query2);
+        }
+        catch (SQLException ed) {
+
+            System.err.println(ed);
+
+        }
+
+    }
+
+
+
+
+
+
+}
