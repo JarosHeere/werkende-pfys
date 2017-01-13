f60781282da3cac2604767e99d05fe054025a09d
JarosHeere <jaros.heere2@gmail.com>
2017-01-13 16:10:51 +0100

    ?

--- src/pyfs/Foundd.java
+++ src/pyfs/Foundd.java
@@ -14,10 +14,10 @@ public class Foundd {
 
     mysql Mysql = new mysql();
     Found found1 = new Found();
-    
+
     String[] foundbagage;
-   String[] foundDate;
-   String[] foundLabel;
+    String[] foundDate;
+    String[] foundLabel;
 
     public void setFoundbagage(String[] foundbagage) {
         this.foundbagage = foundbagage;
@@ -30,10 +30,6 @@ public class Foundd {
     public void setFoundLabel(String[] foundLabel) {
         this.foundLabel = foundLabel;
     }
-   
-   
-    
-    
 
     public Foundd() {
     }
@@ -45,19 +41,14 @@ public class Foundd {
         final String USERNAME = Mysql.username();
         final String PASSWORD = Mysql.password();
         final String CONN_STRING = Mysql.urlmysql();
-        
-  
-        int Lugagenr = 2;
 
-       
         try {
 
             conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
             Statement st = conn.createStatement();
-            
 
-            String query = "INSERT INTO luggage (Luggagelnr, Luggagetype, Luggagebrand, Luggagecol, Luggeweight, Luggagewespef, Unr,Pnr, LFDM) VALUES (" + '"' + Lugagenr + '"' + "," + '"' + foundbagage[0] + '"' + "," + '"' + foundbagage[1] + '"' + "," + '"'
-                    + foundbagage[2] + '"' + "," + '"' + foundbagage[3] + '"' + "," + '"' + foundbagage[4]  +'"' + "," + '"' + Unr  + '"' +  "," + '"' + null  +'"' + ",'F'" + " )";
+            String query = "INSERT INTO luggage (Luggagetype, Luggagebrand, Luggagecol, Luggeweight, Luggagewespef, Unr,Pnr, LFDM) VALUES (" + '"' + foundbagage[0] + '"' + "," + '"' + foundbagage[1] + '"' + "," + '"'
+                    + foundbagage[2] + '"' + "," + '"' + foundbagage[3] + '"' + "," + '"' + foundbagage[4] + '"' + "," + '"' + Unr + '"' + "," + '"' + null + '"' + ",'F'" + " )";
 
             st.executeUpdate(query);
 
@@ -77,21 +68,16 @@ public class Foundd {
         final String PASSWORD = Mysql.password();
         final String CONN_STRING = Mysql.urlmysql();
 
-
         try {
 
             conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
             Statement st = conn.createStatement();
-            
-            
 
             String query = ("INSERT INTO dta (date, time, airport, Unr) VALUES (" + '"' + foundDate[0] + '"' + "," + '"' + foundDate[1] + '"' + "," + '"' + foundDate[2] + '"' + " ," + Unr + ")");
 
             //String query = "INSERT INTO dta (date, time, airport, Unr) VALUES (" + '"' + datex[0] + '"' + "," + '"' + datex[1] + '"' + "," + '"' + datex[2] + '"' + "," + '"' + 100 + '"' + " )";
-
             st.executeUpdate(query);
-        }
-        catch (SQLException ed) {
+        } catch (SQLException ed) {
 
             System.err.println(ed);
 
@@ -99,7 +85,7 @@ public class Foundd {
 
     }
 
- public void getLabel(int Unr) {
+    public void getLabel(int Unr) {
 
         Connection conn;                                                            //making connection to database
 
@@ -107,33 +93,22 @@ public class Foundd {
         final String PASSWORD = Mysql.password();
         final String CONN_STRING = Mysql.urlmysql();
 
-
         try {
 
             conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
             Statement st = conn.createStatement();
-            
-            
 
-            String query = ("INSERT INTO flight (Unr, labelnr, flightnr, destin) VALUES (" + '"' + Unr + '"' + "," + "'" + foundLabel[0] + "'" + "," + '"' + foundLabel[1] + '"' + "," + '"' +  null + '"' + ")");
+            String query = ("INSERT INTO flight (Unr, labelnr, flightnr, destin) VALUES (" + '"' + Unr + '"' + "," + "'" + foundLabel[0] + "'" + "," + '"' + foundLabel[1] + '"' + "," + '"' + null + '"' + ")");
             //String query2 = ("INSERT into persoon (Pnr, name, adress, city, zip, country, tel, mail) VALUES (25, " + '"' + info[2] + '"' + ", null, null, null, null, null, null)");
 
-           
-
             st.executeUpdate(query);
-         //  st.executeUpdate(query2);
-        }
-        catch (SQLException ed) {
+            //  st.executeUpdate(query2);
+        } catch (SQLException ed) {
 
             System.err.println(ed);
 
         }
 
     }
-
-
-
-
-
 
 }
