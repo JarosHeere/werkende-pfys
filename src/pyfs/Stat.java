239f6f20cc2543785b794f708a7b756356fb365c
JarosHeere <jaros.heere2@gmail.com>
2017-01-15 16:55:44 +0100

    werkende stats?

--- src/pyfs/Stat.java
+++ src/pyfs/Stat.java
@@ -1,5 +1,10 @@
 package pyfs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,44 +19,103 @@ import javafx.scene.Group;
  */
 public class Stat {
 
-  private PieChart piechart;
+    private PieChart piechart;
+    mysql Mysql = new mysql();
 
     public Stat() {
-        
-        
+
     }
 
     public PieChart OverYear() {
-        
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                new PieChart.Data("Found & destroyed", 22),
-                new PieChart.Data("Still lost", 30));
+        final String USERNAME = Mysql.username();
+        final String PASSWORD = Mysql.password();
+        final String CONN_STRING = Mysql.urlmysql();
+        int count = 0;
+        int count1 = 0;
+        int count2 = 0;
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement stmt = (Statement) conn.createStatement();
+            ResultSet rs5 = stmt.executeQuery("SELECT COUNT(*) AS total FROM luggage where LFDM = L");
+
+            while (rs5.next()) {
+                count = rs5.getInt("total");
+            }
+            ResultSet rs6 = stmt.executeQuery("SELECT COUNT(*) AS total FROM luggage where LFDM = D");
+
+            while (rs6.next()) {
+                count1 = rs6.getInt("total");
+                
+            }
+            ResultSet rs7 = stmt.executeQuery("SELECT COUNT(*) AS total FROM luggage where LFDM = F");
+
+            while (rs7.next()) {
+                count2 = rs7.getInt("total");
+            }
+            count1 = count1 + count2;
+        } catch (SQLException ed) {
+
+            System.err.println(ed);
+
+        }
+
+        ObservableList<PieChart.Data> pieChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Found & destroyed", count1),
+                        new PieChart.Data("Still lost", count));
         final PieChart chart = new PieChart(pieChartData);
         chart.setTitle("Luggage past year");
-      
-              
-        
+
         return chart;
     }
 
     public PieChart CurrentLuggage() {
-        
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                new PieChart.Data("Lost at the moment", 220),
-                new PieChart.Data("Back to owner's", 260),
-                new PieChart.Data("Found", 195));
-               
-               
+
+        final String USERNAME = Mysql.username();
+        final String PASSWORD = Mysql.password();
+        final String CONN_STRING = Mysql.urlmysql();
+        int count = 0;
+        int count1 = 0;
+        int count2 = 0;
+        Connection conn;
+
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement stmt = (Statement) conn.createStatement();
+            ResultSet rs5 = stmt.executeQuery("SELECT COUNT(*) AS total FROM luggage where LFDM = L");
+
+            while (rs5.next()) {
+                count = rs5.getInt("total");
+            }
+            ResultSet rs6 = stmt.executeQuery("SELECT COUNT(*) AS total FROM luggage where LFDM = M");
+
+            while (rs6.next()) {
+                count1 = rs6.getInt("total");
+                count1 = count1 / 2;
+            }
+            ResultSet rs7 = stmt.executeQuery("SELECT COUNT(*) AS total FROM luggage where LFDM = F");
+
+            while (rs7.next()) {
+                count2 = rs7.getInt("total");
+            }
+
+        } catch (SQLException ed) {
+
+            System.err.println(ed);
+
+        }
+
+        ObservableList<PieChart.Data> pieChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Lost at the moment", count),
+                        new PieChart.Data("Back to owner's", count1),
+                        new PieChart.Data("Found", count2));
+
         final PieChart chart2 = new PieChart(pieChartData);
         chart2.setTitle("Current luggage");
-        
+
         return chart2;
     }
-    
-    
-    
-    
 
 }
